9341f1ed98b38510299192991c6e530b2823540c
s surjith <surjith264@gmail.com>
2024-12-28 12:04:59 +0530

    project completed

---
+++ src/main/java/eventapp/Delete.java
@@ -0,0 +1,45 @@
+package eventapp;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+@WebServlet(urlPatterns = "/delete")
+public class Delete extends HttpServlet {
+	
+	@Override
+	public void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		int id=Integer.parseInt(req.getParameter("id"));
+      try {
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/event_management_system", "root","root");
+		PreparedStatement ps=con.prepareStatement("delete from event where id=?");
+		ps.setInt(1,id);
+		int row=ps.executeUpdate();
+	   
+	    
+	    if(row==1) {
+	    	PrintWriter pw=resp.getWriter();
+	    	pw.write("<h1>Event Deleted Successfullly</h1>");
+	    }
+	    RequestDispatcher rd=req.getRequestDispatcher("allevents.jsp");
+	    rd.forward(req,resp);
+	    ps.close();
+	    con.close();
+	    
+	} catch (ClassNotFoundException | SQLException e) {
+		
+		e.printStackTrace();
+	}
+	}
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,26 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert Page</title>
+</head>
+<body>
+<h1>Welcome to Event Management System</h1>
+
+<a href="create.jsp"><button>Create Event</button></a>
+<br>
+<br>
+<a href="all"><button>View All Event</button></a>
+
+
+
+
+
+
+
+
+
+</body>
+</html>
